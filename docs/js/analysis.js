32bca702095ccb03e93de3cedbd25f6140b7fd21
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-10-19 02:58:21 +0000

    Test

--- docs/js/analysis.js
+++ docs/js/analysis.js
@@ -45,7 +45,7 @@ window.addEventListener('load', () => {
         
         // set the dimensions and margins of the graph
         const margin = {top: 10, right: 30, bottom: 30, left: 60},
-                width = 460 - margin.left - margin.right,
+                width = 800 - margin.left - margin.right,
                 height = 400 - margin.top - margin.bottom;
 
         // append the svg object to the body of the page
